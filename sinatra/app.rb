bc5d699a109ab9d2bae894a7e1ddce012e11530a
Michael Schats <michael_schats@hotmail.com>
2020-11-09 16:09:43 +0000

    Created an app using sinatra, then have run shotgun. Put a link in our file to open an image of a cat

---
+++ sinatra/app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  'Josh and Michael Pair session'
+end
+
+get '/secret/phase3' do
+   'Running shotgun'
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end 
